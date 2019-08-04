b0b4cb4ff0ca27e5318ec0bda05e2c56eed1ab4b
Arthur Carvalho <carvalho.yoa.arthur@gmail.com>
2019-08-04 20:43:53 -0300

    timer-app-functions: Moved functions into own source files.

---
+++ timer-app-functions/functions/handlers/users.js
@@ -0,0 +1,94 @@
+const { db } = require('../firebase/admin');
+
+const { config } = require('../config');
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const { validateSignupData, validateLoginData } = require('../utils/validators');
+
+// Signup
+exports.signup = (request, response) => {
+  const newUser = {
+    displayName: request.body.displayName,
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    country: request.body.country
+  };
+
+  const { errors, valid } = validateSignupData(newUser);
+
+  if (!valid) return response.status(400).json(errors);
+
+  const defaultImg = "no-img.png";
+
+  let uid, token;
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      uid = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userData = {
+        displayName: newUser.displayName,
+        email: newUser.email,
+        country: newUser.country,
+        photoUrl: defaultImg,
+        defaultWorkspace: "",
+        settings: [
+          "warnLongRunningTimers",
+          "groupSimilarEntries",
+          "showTimerOnTitleBar"
+        ],
+        timeSettings: {
+          dateFormat: "MM/DD/YY",
+          firstDayOfWeek: "Monday",
+          timeZone: "UTC-3",
+          timeFormat: "12hour"
+        },
+        createdAt: new Date().toISOString()
+      };
+      return db.doc(`/users/${uid}`).set(userData);
+    })
+    .then(() => {
+      return response.status(201).json({ token });
+    })
+    .catch(error => {
+      return response
+        .status(400)
+        .json({ error: error.code, message: error.message });
+    });
+};
+
+// Login
+exports.login = (request, response) => {
+  const user = {
+    email: request.body.email,
+    password: request.body.password
+  };
+
+  const { errors, valid } = validateLoginData(user);
+
+  if (!valid) return response.status(400).json(errors);
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return response.json({ token });
+    })
+    .catch(error => {
+      console.error(error);
+      if(error.code === 'auth/wrong-password'){
+        return response.status(403).json({ general: 'Wrong credentials, please try again'});
+      } else return response.status(500).json({ error: error.code, message: error.message });
+    });
+};
